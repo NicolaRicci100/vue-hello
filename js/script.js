6c595d18e5fe71d3261fa25f4edd9dc8f837124b
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-16 15:14:07 +0200

    inserimento elementi tramite Vue

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            vueImage: 'img/vue-js.png'
         }
     }
-}).mount('#app')
+}).mount('#root');
